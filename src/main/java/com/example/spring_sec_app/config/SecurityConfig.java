f06373725e7982f7bf6f196737b448c88ea663b3
Ivan Ivanov <79523701529@yandex.ru>
2022-10-04 12:59:03 +0300

    spring security with no authProvider

--- src/main/java/com/example/spring_sec_app/config/SecurityConfig.java
+++ src/main/java/com/example/spring_sec_app/config/SecurityConfig.java
@@ -1,20 +1,27 @@
 package com.example.spring_sec_app.config;
 
-import com.example.spring_sec_app.security.AuthProviderImpl;
+import com.example.spring_sec_app.services.PersonDetailsService;
+import org.hibernate.bytecode.enhance.internal.tracker.NoopCollectionTracker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final AuthProviderImpl authProvider;
-@Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    private final PersonDetailsService personDetailsService;
+    @Autowired
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
-    protected void configure(AuthenticationManagerBuilder auth){
-        auth.authenticationProvider(authProvider);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
+    }
+
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
--- src/main/java/com/example/spring_sec_app/controllers/PersonController.java
+++ src/main/java/com/example/spring_sec_app/controllers/PersonController.java
@@ -1,5 +1,8 @@
 package com.example.spring_sec_app.controllers;
 
+import com.example.spring_sec_app.security.PersonDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,6 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class PersonController {
     @GetMapping("/hello")
     public String hello(){
+        return "hello.html";
+    }
+
+    @GetMapping("/showInfo")
+    public String showInfo(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        System.out.println(personDetails.getPerson());
         return "hello.html";
     }
 }
