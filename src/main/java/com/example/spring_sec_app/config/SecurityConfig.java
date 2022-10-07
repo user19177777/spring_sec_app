5d93c5e0f087cf3f60f7f7fc0e9846a1bb8f2417
Ivan Ivanov <79523701529@yandex.ru>
2022-10-07 16:03:21 +0300

    add custom registration page

--- src/main/java/com/example/spring_sec_app/config/SecurityConfig.java
+++ src/main/java/com/example/spring_sec_app/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.hibernate.bytecode.enhance.internal.tracker.NoopCollectionTracker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -18,6 +19,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.personDetailsService = personDetailsService;
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //конф. сам Spring Security
+        //конф. авторизацию
+        http.csrf().disable()//отключаем защиту от межсайтовой подделки запросов
+                .authorizeRequests()
+                .antMatchers("/auth/login","/error").permitAll()//доступ для всех пользователей
+                .anyRequest().authenticated()//доступ к остальным страницам только для аутентифицированных
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello",true)
+                .failureUrl("/auth/login?error");
+    }
+
+    //настраиваем аутентификацию
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personDetailsService);
     }
