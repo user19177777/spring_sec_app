94eadc61dcd9887e85a61cf1afd7a30d8a18bcdd
Ivan Ivanov <79523701529@yandex.ru>
2022-10-10 14:08:15 +0300

    add encryption password

--- src/main/java/com/example/spring_sec_app/config/SecurityConfig.java
+++ src/main/java/com/example/spring_sec_app/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -33,18 +34,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .defaultSuccessUrl("/hello",true)
                 .failureUrl("/auth/login?error")
                 .and()
-        .logout()
-        .logoutUrl("/logout")
-        .logoutSuccessUrl("/auth/login");
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/auth/login");
     }
 
     //настраиваем аутентификацию
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(personDetailsService);
+        auth.userDetailsService(personDetailsService)
+        .passwordEncoder(getPasswordEncoder());
     }
 
     @Bean
     public PasswordEncoder getPasswordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/example/spring_sec_app/services/RegistrationService.java
+++ src/main/java/com/example/spring_sec_app/services/RegistrationService.java
@@ -3,18 +3,22 @@ package com.example.spring_sec_app.services;
 import com.example.spring_sec_app.models.Person;
 import com.example.spring_sec_app.repo.PeopleRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RegistrationService {
     private final PeopleRepo peopleRepo;
+    private final PasswordEncoder passwordEncoder;
     @Autowired
-    public RegistrationService(PeopleRepo peopleRepo) {
+    public RegistrationService(PeopleRepo peopleRepo, PasswordEncoder passwordEncoder) {
         this.peopleRepo = peopleRepo;
+        this.passwordEncoder = passwordEncoder;
     }
 
 
     public void register(Person person){
+        person.setPassword(passwordEncoder.encode(person.getPassword()));
         peopleRepo.save(person);
     }
 }
