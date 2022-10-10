2a44204514ed7d7574a8fe29d3b3eb048a43e52c
Ivan Ivanov <79523701529@yandex.ru>
2022-10-10 16:37:46 +0300

    add protection from csrf

--- src/main/java/com/example/spring_sec_app/config/SecurityConfig.java
+++ src/main/java/com/example/spring_sec_app/config/SecurityConfig.java
@@ -24,8 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         //конф. сам Spring Security
         //конф. авторизацию
-        http.csrf().disable()//отключаем защиту от межсайтовой подделки запросов
-                .authorizeRequests()
+        http.authorizeRequests()
                 .antMatchers("/auth/login", "/auth/registration","/error").permitAll()//доступ для всех пользователей
                 .anyRequest().authenticated()//доступ к остальным страницам только для аутентифицированных
                 .and()
